2e6e0d768fdeef392f309497cfdf52f20bcc2b32
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-15 11:40:22 +0530

    Update variables.tf

--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -3,4 +3,7 @@ variable "topic_name" {
   type        = string
   default     = "welcome-email-topic"
 }
-
+variable "lambda_function_arn" {
+  description = "ARN of the Lambda function to subscribe"
+  type        = string
+}
