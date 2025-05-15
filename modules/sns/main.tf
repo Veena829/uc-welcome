99c9b51a1cd63542f7b0b3eda431f355b8b0511d
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-15 13:09:12 +0530

    Update main.tf

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -7,7 +7,7 @@ resource "aws_sns_topic" "welcome_email_topic" {
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.welcome_email_topic.arn
   protocol  = "email"  # You can change this to any other protocol (e.g., "http", "https", "lambda", etc.)
-  endpoint  = "your-email@example.com"  # Replace with the actual email address you want to subscribe
+  endpoint  = "veenayaraddi98@gmail.com"  # Replace with the actual email address you want to subscribe
 }
 
 # Output the SNS topic ARN
